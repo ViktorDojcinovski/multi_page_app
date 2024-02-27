9d8ffdade2713fe2177e29a0cf4ad25db9247822
ViktorDojchinovski <viktorjano@yahoo.com>
2024-02-27 10:37:36 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { Switch, Route } from "react-router-dom";
+
+import About from "./pages/About/About.component";
+import Contact from "./pages/Contact/Contact.component";
+import "./App.css";
 
 function App() {
+  // <Routes />
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Switch>
+      <Route component={About} path="/about" />
+      <Route component={Contact} path="/contact" />
+    </Switch>
   );
 }
 
